165b1860e342146dccc9ba8124be6d97a06be497
Ajaykarki13 <115032238+Ajaykarki13@users.noreply.github.com>
2023-03-10 00:22:06 +0530

    cart update

--- mycart.js
+++ mycart.js
@@ -1,49 +1,53 @@
-let data = JSON.parse(localStorage.getItem("all"))
+
 let cartdata = JSON.parse(localStorage.getItem("cart"))
-data.map((item)=>{
+
+ // mapping cart data on screen 
+
     cartdata.map((x)=>{
-        if(item.id===x.id)
-        {
             let wrapper = document.querySelector(".products_wrapper")
               wrapper.innerHTML +=
                 `
             <div class="item">
-            <img src="${item.image}" alt="Item" />
+            <img src="${x.image}" alt="Item" />
             <div class="info">
               <div class="row">
-              <div>Product: ${item.title.split(' ',2).join()}</div>
-                <div class="price">Price: ₹${item.price}</div>
+              <div>Product: ${x.title.split(' ',2).join()}</div>
+                <div class="price">Price: ₹${x.price}</div>
                
               </div>
-              <div class="row">Rating: ${item.rating['rate']}</div>
+              <div class="row">Rating: ${x.rating['rate']}</div>
             </div>
             <button class="remove" data-product-id=${x.id} id="removeitem">Remove item</button>
           </div>
           `                             
-         
+         // checkout content
 
          let checkoutlist = document.querySelector(".checkout")
          checkoutlist.innerHTML +=
          `
-        <li>${item.title} :- ₹${item.price} </li>
-         `
-        };
-        })
-    });
-//=======>>>>>>>payment process<<<<<<<<<==========//
+        <li>${x.title} :- ₹${x.price} </li>
 
-var totalPrice = cartdata.reduce((sum,x)=> { return sum+x.price},0);
-document.querySelector("#total").innerHTML=`Total Amount:- ₹ ${totalPrice}`;
+         `
+         totalPrice(cartdata);
+        });
+       
+
+//=======>>>>>>>payment process<<<<<<<<<==========//
+var amount ;
+function totalPrice(data)
+{
+var total = data.reduce((sum,x)=> { return sum+x.price},0);
+document.querySelector("#total").innerHTML=`Total Amount:- ₹ ${total}`;
+amount = total;
+}
 
 //checkout button on click
-document.getElementById("rzp").onclick = function (e) 
-  {
-    
-   
+document.querySelector("#rzp").onclick = function (e) 
+  { 
 //razor pay object
     var option = {
       key: "rzp_test_9OXl2wK2vH03Va", // Enter the Key ID generated from the Dashboard
-      amount: totalPrice*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      amount: amount*100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
       currency: "INR",
       name: "Me Shop",
       description: "This is your order", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
@@ -64,26 +68,58 @@ document.getElementById("rzp").onclick = function (e)
     e.preventDefault();
   };
     
-/*
-    let wrapper = document.querySelector(".products_wrapper")
-    wrapper.addEventListener('click', (event) => {
+// remove product btn functionality
 
-      const remove = event.target.closest('#removeitem');   
-    
-      if (remove) {
-        const id = remove.getAttribute('data-product-id');  // finding the data id and matching with the product id to send that particular id
-        let allcart = JSON.parse(localStorage.getItem("cart"))
-        const product = allcart.find((product) => product.id == id); //closest is used so that the functionality works when products are filtred
+   let wrapper = document.querySelector(".products_wrapper")
+    wrapper.addEventListener('click', (event) => {
+      wrapper.innerHTML = '';
+      let checkoutlist = document.querySelector(".checkout")
+      checkoutlist.innerHTML = '';
+      const removebtn = event.target.closest('#removeitem');   //accessing the remove button of cards
+      if (removebtn) {
+        const id = removebtn.getAttribute('data-product-id');  // finding the data id and matching with the product id to send that particular id
+         
+        const index = cartdata.findIndex((product) => product.id == id); 
+        console.log(index);
+        if (index>-1) {
+          cartdata.splice(index,1);
         
-        if (product) {
-         // wrapper.innerHTML=''
-         // addToCartBtn.style.backgroundColor = 'green'
-          cartdata.pop(product);
-         // localStorage.setItem('cart', JSON.stringify(cart));
+          localStorage.setItem('cart', JSON.stringify(cartdata));
+          let newcart = JSON.parse(localStorage.getItem("cart"))
+         newcart.map((x)=>{
+          
+           {
+               let wrapper = document.querySelector(".products_wrapper")
+                 wrapper.innerHTML +=
+                   `
+               <div class="item">
+               <img src="${x.image}" alt="Item" />
+               <div class="info">
+                 <div class="row">
+                 <div>Product: ${x.title.split(' ',2).join()}</div>
+                   <div class="price">Price: ₹${x.price}</div>
+                  
+                 </div>
+                 <div class="row">Rating: ${x.rating['rate']}</div>
+               </div>
+               <button class="remove" data-product-id=${x.id} id="removeitem">Remove item</button>
+             </div>
+             `                             
+          
+          //  let checkoutlist
+            checkoutlist.innerHTML +=
+            `
+           <li>${x.title} :- ₹${x.price} </li>
+            `
+           };
+           totalPrice(newcart)
+           })
+
         }
     
       }
     });
-*/
+
+
 
 
